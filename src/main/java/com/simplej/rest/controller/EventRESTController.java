bec82fbe30392c834f079279e99dd26daab1d45b
vvendemia@jibe.com <vvendemia@jibe.com>
2018-05-22 11:48:02 -0400

    Add Event endpoints

--- src/main/java/com/simplej/rest/controller/EventRESTController.java
+++ src/main/java/com/simplej/rest/controller/EventRESTController.java
@@ -1,13 +1,23 @@
 package com.simplej.rest.controller;
 
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.util.ArrayList;
+import java.util.Set;
+import javax.validation.ConstraintViolation;
 import javax.validation.Validator;
+import javax.ws.rs.DELETE;
 import javax.ws.rs.GET;
+import javax.ws.rs.POST;
+import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
-
+import javax.ws.rs.core.Response.Status;
 import com.simplej.rest.dao.EventDB;
+import com.simplej.rest.entity.Event;
 
 @Path("/events")
 @Produces(MediaType.APPLICATION_JSON)
@@ -23,11 +33,64 @@ public class EventRESTController {
         return Response.ok(EventDB.getEvents()).build();
     }
 	
-	// GET BY ID 
-	
-	// POST 
-	
-	// PUT 
-	
-	// DELETE 
+    @GET
+    @Path("/{id}")
+    public Response getEventById(@PathParam("id") Integer id) {
+        Event event = EventDB.getEvent(id);
+        if (event != null)
+            return Response.ok(event).build();
+        else
+            return Response.status(Status.NOT_FOUND).build();
+    }
+ 
+	@POST
+	public Response createEvent(Event event) throws URISyntaxException {
+		// validation
+		Set<ConstraintViolation<Event>> violations = validator.validate(event);
+		Event e = EventDB.getEvent(event.getId());
+		if (violations.size() > 0) {
+			ArrayList<String> validationMessages = new ArrayList<String>();
+			for (ConstraintViolation<Event> violation : violations) {
+				validationMessages.add(violation.getPropertyPath().toString() + ": " + violation.getMessage());
+			}
+			return Response.status(Status.BAD_REQUEST).entity(validationMessages).build();
+		}
+		if (e != null) {
+			EventDB.updateEvent(event.getId(), event);
+			return Response.created(new URI("/events/" + event.getId())).build();
+		} else
+			return Response.status(Status.NOT_FOUND).build();
+	}
+ 
+    @PUT
+    @Path("/{id}")
+    public Response updateEventById(@PathParam("id") Integer id, Event event) {
+        // validation
+        Set<ConstraintViolation<Event>> violations = validator.validate(event);
+        Event e = EventDB.getEvent(event.getId());
+        if (violations.size() > 0) {
+            ArrayList<String> validationMessages = new ArrayList<String>();
+            for (ConstraintViolation<Event> violation : violations) {
+                validationMessages.add(violation.getPropertyPath().toString() + ": " + violation.getMessage());
+            }
+            return Response.status(Status.BAD_REQUEST).entity(validationMessages).build();
+        }
+        if (e != null) {
+            event.setId(id);
+            EventDB.updateEvent(id, event);
+            return Response.ok(event).build();
+        } else
+            return Response.status(Status.NOT_FOUND).build();
+    }
+ 
+    @DELETE
+    @Path("/{id}")
+    public Response removeEventById(@PathParam("id") Integer id) {
+    	Event event = EventDB.getEvent(id);
+        if (event != null) {
+        	EventDB.removeEvent(id);
+            return Response.ok().build();
+        } else
+            return Response.status(Status.NOT_FOUND).build();
+    }
 }
--- src/main/java/com/simplej/rest/dao/EventDB.java
+++ src/main/java/com/simplej/rest/dao/EventDB.java
@@ -9,16 +9,30 @@ import com.simplej.rest.entity.Event;
 
 public class EventDB {
 	public static HashMap<Integer, Event> events = new HashMap<>();
-    static{
-    	events.put(1, new Event(1, "Event 1", todaysDate(), 50, "TEST DESCRIPTION 1", "red"));
-    	events.put(2, new Event(2, "Event 2", todaysDate(), 50, "TEST DESCRIPTION 2", "blue"));
-    	events.put(3, new Event(3, "Event 3", todaysDate(), 50, "TEST DESCRIPTION 3", "green"));
-    }
-    
-    public static List<Event> getEvents(){
-        return new ArrayList<Event>(events.values());
-    }
-    
+	static {
+		events.put(1, new Event(1, "Event 1", todaysDate(), 50, "TEST DESCRIPTION 1", "red"));
+		events.put(2, new Event(2, "Event 2", todaysDate(), 50, "TEST DESCRIPTION 2", "blue"));
+		events.put(3, new Event(3, "Event 3", todaysDate(), 50, "TEST DESCRIPTION 3", "green"));
+	}
+
+	public static List<Event> getEvents() {
+		return new ArrayList<Event>(events.values());
+	}
+
+	public static Event getEvent(Integer id) {
+		return events.get(id);
+	}
+
+	public static void updateEvent(Integer id, Event event) {
+		events.put(id, event);
+	}
+
+	public static void removeEvent(Integer id) {
+		events.remove(id);
+	}
+
+	// private methods
+
 	private static Date todaysDate() {
 		Calendar today = Calendar.getInstance();
 		today.set(Calendar.HOUR_OF_DAY, 0);
